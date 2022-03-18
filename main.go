181adb7fbabd29454d088c95525b8f18352cfd5d
earlgray <earlgray283@gmail.com>
2022-03-18 11:59:46 +0900

    feat: 動かないオプションたちを追加

--- main.go
+++ main.go
@@ -2,42 +2,104 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"fmt"
 	"go/format"
 	"go/token"
+	"io"
 	"log"
 	"os"
 
 	"golang.org/x/tools/imports"
 )
 
-const (
-	filename  string = "examples/samber_lo.go"
-	dstPath   string = "/Users/earlgray/Workspace/Intern/Mercari/goder/tmp/single_samber_lo.go"
-	suffixLen int    = 8
+const suffixLen int = 8
+
+var (
+	dst                 io.Writer = os.Stdout // 出力先
+	overwrite           bool                  // 上書き
+	onlyConvertGenerics bool                  // ジェネリクスの変換のみ
+	onlyConvertExpkgs   bool                  // 外部パッケージの変換のみ
 )
 
+func init() {
+	flag.BoolVar(&overwrite, "w", false, "write result to (source) file instead of stdout")
+	flag.BoolVar(&onlyConvertGenerics, "g", false, "only convert generics")
+	flag.BoolVar(&onlyConvertExpkgs, "e", false, "only convert external packages")
+	flag.Usage = func() {
+		fmt.Println("Usage:")
+		fmt.Println()
+		fmt.Println("  goder [Options] <source_file>")
+		fmt.Println()
+		fmt.Println("Options:")
+		fmt.Println()
+		flag.PrintDefaults()
+	}
+}
+
 func main() {
-	srcBytes, err := os.ReadFile(filename)
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	filename := flag.Arg(0)
+	srcFile, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	src, err := ReplaceExternalPkgs(srcBytes)
-	if err != nil {
+	defer srcFile.Close()
+	if overwrite {
+		dst = srcFile
+	}
+
+	srcBytes := &bytes.Buffer{}
+	if _, err := io.Copy(srcBytes, srcFile); err != nil {
 		log.Fatal(err)
 	}
 
-	src2, err := ConvertGenerics(src)
+	if onlyConvertExpkgs {
+		convertedSrc, err := ConvertExternalPkgs(srcBytes.Bytes())
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := dst.Write(convertedSrc); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	if onlyConvertGenerics {
+		convertedSrc, err := ConvertGenerics(srcBytes.Bytes())
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := dst.Write(convertedSrc); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	convertedSrc, err := convertAll(srcBytes.Bytes())
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	if err := createFileWithBytes(dstPath, src2); err != nil {
+	if _, err := dst.Write(convertedSrc); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func createFileWithBytes(filename string, data []byte) error {
-	f, err := os.Create(filename)
+func convertAll(src []byte) ([]byte, error) {
+	noExpkgsSrc, err := ConvertExternalPkgs(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ConvertGenerics(noExpkgsSrc)
+}
+
+// create back-up file.
+func createBak(bakPath string, data []byte) error {
+	f, err := os.Create(bakPath + ".bak")
 	if err != nil {
 		return err
 	}
@@ -51,5 +113,5 @@ func formatAst(f any, fset *token.FileSet) ([]byte, error) {
 	if err := format.Node(buf, fset, f); err != nil {
 		return nil, err
 	}
-	return imports.Process("", buf.Bytes(), nil)
+	return imports.Process("", buf.Bytes(), &imports.Options{})
 }
